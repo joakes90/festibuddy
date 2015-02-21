0e0cb66677333f02aaeb07c4f737759b4ca35429
Justin Oakes <justin@justinoak.es>
2015-02-21 03:58:30 -0600

    set up sliding view controller and table view controller

---
+++ Festi Buddy/Festi Buddy-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ECPercentDrivenInteractiveTransition.h"
+#import "ECSlidingAnimationController.h"
+#import "ECSlidingConstants.h"
+#import "ECSlidingInteractiveTransition.h"
+#import "ECSlidingSegue.h"
+#import "ECSlidingViewController.h"
+#import "UIViewController+ECSlidingViewController.h"
